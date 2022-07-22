2ce0ce015d6c0d2982dc356725ab1c86964cc761
kennersonpv <kennersondudu@hotmail.com>
2022-07-22 17:15:15 -0300

    Add project files.

---
+++ Post.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KenneOverflow
+{
+    class Post
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime Creation { get; set; }
+        private int VoteCount { get; set; }
+
+        public Post(string title, string description)
+        {
+            Title = title;
+            Description = description;
+            Creation = DateTime.Now;
+            VoteCount = 0;
+        }
+
+        public int VoteUp()
+        {
+            VoteCount = VoteCount + 1;
+            return VoteCount;
+        }
+
+        public int VoteDown()
+        {
+            if (VoteCount > 0)
+            { 
+                VoteCount = VoteCount - 1; 
+            }
+            return VoteCount;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace KenneOverflow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var title = "How to create a class?";
+            var description = "I'm a student and I don't know how to create a class.Can someone help me?";
+            var post = new Post(title, description);
+            Console.WriteLine(post.Title);
+            Console.WriteLine(post.Description);
+            Console.WriteLine(post.Creation);
+            Console.WriteLine(post.VoteUp());
+            Console.WriteLine(post.VoteUp());
+            Console.WriteLine(post.VoteUp());
+            Console.WriteLine(post.VoteDown());
+            Console.WriteLine(post.VoteDown());
+            Console.WriteLine(post.VoteDown());
+            Console.WriteLine(post.VoteDown());
+            Console.WriteLine(post.VoteDown());
+        }
+    }
+}
